4a543da7599c0e45de636dd69fda181adb372231
psychoactiv <pritam54majumdar@gmail.com>
2022-03-14 13:28:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-comm Project</h1>
+    </div>
+  );
+}
+
+export default App;
